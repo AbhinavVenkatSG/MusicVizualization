19a07f3085d30975de9ec067b2cf00d89568e8b9
Abhinav Venkat Sivakumar Gayathiri <Asivakumargayat1253@conestogac.on.ca>
2025-07-30 23:56:36 -0400

    Removing mem_file_free and mem file allocation
    #24 #14

--- MusicVizualization/fileops.c
+++ MusicVizualization/fileops.c
@@ -4,29 +4,7 @@
 
 
 
-int memfilepath(char* inputarray[MAX_FILEPATH_RECORDED]) {
-	for (int i = 0; i < MAX_FILEPATH_RECORDED; i++) {
-		inputarray[i] = (char*)calloc(MAX_FILE_SIZE, 1);
-		if (inputarray[i] == NULL) {
-			fprintf(stderr, "Memory Allocator Failed (MemFilepath)\n");
-			return -1;
-		}
-	}
 
-	return 0;
-}
-
-
-
-int Mem_filepath_free(char* inputarray[MAX_FILEPATH_RECORDED]) {
-	for (int i = 0; i < MAX_FILEPATH_RECORDED; i++) {
-		if (inputarray[i] != NULL) {
-			free(inputarray[i]);
-			inputarray[i] = NULL; 
-		}
-	}
-	return 0;
-}
 
 
 int Create_File(char* filename, char* inputarray[MAX_FILEPATH_RECORDED]) {
--- MusicVizualization/fileops.h
+++ MusicVizualization/fileops.h
@@ -6,10 +6,8 @@
 #include <string.h>
 #define MAX_FILE_CHAR_LENGTH 512
 
-int memfilepath(char* inputarray[MAX_FILEPATH_RECORDED]);
+
 void Drag_N_Drop(char filearray[MAX_FILEPATH_RECORDED][MAX_PATH_LENGTH], int* filepathcounter);
 
-
-int Mem_filepath_free(char* inputarray[MAX_FILEPATH_RECORDED]);
 
 int Create_File(char* filename, char* inputarray[MAX_FILEPATH_RECORDED]);
