250d93525cf67e7173dfaa648180ef54565d6cf2
Abhinav Venkat Sivakumar Gayathiri <Asivakumargayat1253@conestogac.on.ca>
2025-08-07 08:25:40 -0400

    implmented Suffle function #21 #19 #22 #20 #16

--- MusicVizualization/source.c
+++ MusicVizualization/source.c
@@ -1,74 +1,101 @@
-﻿#include <assert.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
 #include <raylib.h>
 #include "plug.h"
 
-#define MAX_SONGS 20
+#define MAX_SONGS 100
 #define MAX_PATH_LEN 512
 
-char playlist[MAX_SONGS][MAX_PATH_LEN] = { 0 };
-const char* playlistPtrs[MAX_SONGS] = { 0 };
-int songCount = 0;
-
+const char* playlist[MAX_SONGS] = { 0 };
+int playlist_size = 0;
 int currentSongIndex = 0;
+int shuffleMode = 0;
+
 Plug plug = { 0 };
 
-void play_next_song(Plug* plug, const char* playlist[], int* currentIndex, int maxSongs) {
-    *currentIndex = (*currentIndex + 1) % maxSongs;
-    plug_init(plug, playlist[*currentIndex]);
-}
-
-void LoadPlaylistFromTextFile(const char* promptMessage) {
-    char fileName[256];
-    printf("%s: ", promptMessage);
-    if (fgets(fileName, sizeof(fileName), stdin) == NULL) {
-        fprintf(stderr, "Error reading file name.\n");
-        exit(1);
-    }
-
-    size_t len = strlen(fileName);
-    if (len > 0 && fileName[len - 1] == '\n') {
-        fileName[len - 1] = '\0';
-    }
-
-    FILE* file = fopen(fileName, "r");
+int load_playlist_from_file(const char* filename) {
+    FILE* file = fopen(filename, "r");
     if (!file) {
-        fprintf(stderr, "Could not open file: %s\n", fileName);
-        exit(1);
+        fprintf(stderr, "Failed to open playlist file: %s\n", filename);
+        return 0;
     }
 
     char line[MAX_PATH_LEN];
-    while (fgets(line, sizeof(line), file) && songCount < MAX_SONGS) {
-        size_t l = strlen(line);
-        if (l > 0 && (line[l - 1] == '\n' || line[l - 1] == '\r')) {
-            line[l - 1] = '\0';
+    playlist_size = 0;
+    while (fgets(line, sizeof(line), file) && playlist_size < MAX_SONGS) {
+        line[strcspn(line, "\r\n")] = 0;
+        if (line[0] == '\0') continue;
+
+        char* path = malloc(strlen(line) + 1);
+        if (!path) {
+            fprintf(stderr, "Memory allocation failed\n");
+            fclose(file);
+            return 0;
         }
-
-        TextCopy(playlist[songCount], line);
-        playlistPtrs[songCount] = playlist[songCount];
-        songCount++;
+        strcpy(path, line);
+        playlist[playlist_size++] = path;
     }
-
     fclose(file);
-
-    if (songCount == 0) {
-        fprintf(stderr, "No valid song paths found in the file.\n");
-        exit(1);
-    }
-
-    printf("Loaded %d song(s) from %s\n", songCount, fileName);
+    return playlist_size;
 }
 
-int main(int argc, char** argv) {
-    LoadPlaylistFromTextFile("Enter path to playlist .txt file");
+void free_playlist() {
+    for (int i = 0; i < playlist_size; i++) {
+        free((void*)playlist[i]);
+        playlist[i] = NULL;
+    }
+    playlist_size = 0;
+}
 
-    InitWindow(800, 600, "Music Visualizer");
-    SetTargetFPS(60);
+void play_next_song() {
+    if (shuffleMode) {
+        if (playlist_size > 1) {
+            int next;
+            do {
+                next = rand() % playlist_size;
+            } while (next == currentSongIndex);
+            currentSongIndex = next;
+        }
+    }
+    else {
+        currentSongIndex = (currentSongIndex + 1) % playlist_size;
+    }
+    plug_init(&plug, playlist[currentSongIndex]);
+}
+
+int main(void) {
+    srand((unsigned int)time(NULL));
+
+    char playlistFilePath[MAX_PATH_LEN];
+    printf("Enter path to playlist text file: ");
+    if (fgets(playlistFilePath, sizeof(playlistFilePath), stdin) == NULL) {
+        fprintf(stderr, "Failed to read input\n");
+        return 1;
+    }
+    playlistFilePath[strcspn(playlistFilePath, "\r\n")] = 0;
+
+    if (!load_playlist_from_file(playlistFilePath)) {
+        fprintf(stderr, "No songs loaded. Exiting.\n");
+        return 1;
+    }
+
+    if (playlist_size == 0) {
+        fprintf(stderr, "Playlist is empty. Exiting.\n");
+        return 1;
+    }
+
+    InitWindow(800, 600, "Music Player with Visualizer");
     InitAudioDevice();
+    SetTargetFPS(60);
 
-    plug_init(&plug, playlistPtrs[currentSongIndex]);
+    currentSongIndex = 0;
+    plug_init(&plug, playlist[currentSongIndex]);
+
+    shuffleMode = 0;
 
     bool songEnded = false;
 
@@ -81,25 +108,36 @@ int main(int argc, char** argv) {
 
         if (done && !songEnded) {
             StopMusicStream(plug.music);
-            play_next_song(&plug, playlistPtrs, &currentSongIndex, songCount);
+            play_next_song();
             songEnded = true;
         }
         else if (!done) {
             songEnded = false;
         }
 
-        if (IsKeyPressed(KEY_R)) {
-            currentSongIndex = 0;
-            plug_init(&plug, playlistPtrs[currentSongIndex]);
-        }
-
         if (IsKeyPressed(KEY_N)) {
             StopMusicStream(plug.music);
-            play_next_song(&plug, playlistPtrs, &currentSongIndex, songCount);
+            play_next_song();
+            songEnded = false;
+        }
+        if (IsKeyPressed(KEY_R)) {
+            StopMusicStream(plug.music);
+            plug_init(&plug, playlist[currentSongIndex]);
+            songEnded = false;
+        }
+        if (IsKeyPressed(KEY_S)) {
+            shuffleMode = !shuffleMode;
+            printf("Shuffle mode %s\n", shuffleMode ? "ON" : "OFF");
+        }
+        if (IsKeyPressed(KEY_ESCAPE)) {
+            break;
         }
     }
 
+    UnloadMusicStream(plug.music);
     CloseAudioDevice();
     CloseWindow();
+    free_playlist();
+
     return 0;
 }
